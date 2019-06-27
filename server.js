82bf7350928f717c08b0cbc5c875c35a2db70e81
RomanBogatikov <ivaroman2000@gmail.com>
2019-06-27 18:08:03 -0400

    deploy

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 // MIDDLEWARE
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-
+// Serve any static files
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // Controllers
 const carsController = require('./controllers/cars');
@@ -34,21 +35,6 @@ app.get('/', (req, res) => {
   console.log(req.headers);
   res.send('hello cars');
 });
-
-
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-} else {
-  // to serve static files (images) to the Client
-  app.use(express.static('static'));
-}
-
-
 
 
 
@@ -599,5 +585,9 @@ app.get('/seed', (req, res) => {
   ])
 })
 
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
