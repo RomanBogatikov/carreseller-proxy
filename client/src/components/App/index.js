42fb556688827b7a87c229a6d6ac2ce3da20ec78
RomanBogatikov <ivaroman2000@gmail.com>
2019-07-03 21:24:30 -0400

    added media query for smaller screen sizes (need to adjust image width)

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -58,15 +58,16 @@ class App extends Component {
             <legend>
               Car Detail Info
             </legend>
-
-            <div><b>Model: </b>{clicked_car.model}</div>
-            <div><b>Year: </b>{clicked_car.year}</div>
-            <div><b>Producer: </b>{clicked_car.producer}</div>
-            <div><b>Price, $: </b>{clicked_car.price}</div>
-            <div><b>Owner: </b>{clicked_car.owner}</div>
-            <div><b>Tel/Mobile: </b>{clicked_car['tel/mobile']}</div>
-            <div><b>Mileage: </b>{clicked_car.mileage}</div>
-            <div><b>Registered: </b>{clicked_car.registered}</div>
+            <div className="car_details">
+              <div><b>Model: </b>{clicked_car.model}</div>
+              <div><b>Year: </b>{clicked_car.year}</div>
+              <div><b>Producer: </b>{clicked_car.producer}</div>
+              <div><b>Price, $: </b>{clicked_car.price}</div>
+              <div><b>Owner: </b>{clicked_car.owner}</div>
+              <div><b>Tel/Mobile: </b>{clicked_car['tel/mobile']}</div>
+              <div><b>Mileage: </b>{clicked_car.mileage}</div>
+              <div><b>Registered: </b>{clicked_car.registered}</div>
+            </div>
           </fieldset>
 
           <div className="img_container">
